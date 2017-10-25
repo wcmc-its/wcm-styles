2c1472d89c309054d7bb0ee58a8d0d9c8ab29307
Kevin McCormack <kem2028@med.cornell.edu>
2017-10-25 12:23:36 -0400

    Expose jQuery in addition to $

--- webpack.config.js
+++ webpack.config.js
@@ -37,6 +37,9 @@ module.exports = {
         test: require.resolve('jquery'),
         use: [{
           loader: 'expose-loader',
+          options: 'jQuery'
+        },{
+          loader: 'expose-loader',
           options: '$'
         }]
       },
